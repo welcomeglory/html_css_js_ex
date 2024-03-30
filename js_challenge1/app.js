b45fe1717a401312f013b76eec41a9d4c98237b7
welcomeglory <godbu2021@naver.com>
2024-03-30 19:46:38 +0900

    Add files via upload

--- js_challenge1/app.js
+++ js_challenge1/app.js
@@ -36,11 +36,15 @@ const USERNAME_KEY = "username";
 
 //form이 submit되면 우리는 input으로부터 유저정보를 받고,
 function onLoginSubmit(event){
+    //event가 원래하는 행동을 멈춤.(부라우저의 기본 동작을 막아줌. 그동작은 페이지 새로고침임.)
     event.preventDefault();//기본 동작이 실행방지.
+    //form을 다시 숨김.
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    //loginInput.value를 username이라는 변수로 저장.
     const username = loginInput.value;
+    //username값을 username이라는 key와 함께 local storage에 저장.
     localStorage.setItem(USERNAME_KEY,username);
-    //input에서 받은 user를 가진 paintGreetings를 호출.
+    //form안에 있는 input에 입력한 유저명을 받고 paintGreetings를 호출.
     paintGreetings(username);
 }
 
@@ -49,19 +53,25 @@ function paintGreetings(username){
     //아래 두 방법 다 String이랑 변수를 하나로 합쳐주고 있음.
     // greeting.innerText = "Hello " + username;
     //백틱(`)사용으로 띄어쓰기를 편하게 쓸수 있음.
+    //비어 있는 h1 요소 안에 `Hello ${username}`이라는 텍스트를 추가.
     greeting.innerText = `Hello ${username}`;
+    //h1요소로부터 "hidden"이라는 클래스명을 제거.
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
+//js가 USERNAME_KEY를 가지고 local storage를 확인
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null){
     //show the form
+    //loginForm으로부터 HIDDEN_CLASSNAME를 제거
     loginForm.classList.remove(HIDDEN_CLASSNAME);
+    //loginForm에 addEventListener를 더하고, submit을 기다림.
+    //submit event가 발생하면 onLoginSubmit함수가 실행.
     loginForm.addEventListener("submit",onLoginSubmit);
 
 }else{
     //show the greetings
+    //유저정보가 local storge에서 옴.
     paintGreetings(savedUsername);
 } 
 
